3f9ae285446bdeee55488c5a07dfbbc2d34fbc22
Kristof Neirynck <k.neirynck@gmail.com>
2018-11-12 12:26:46 +0100

    Use the currency.txt file

--- src/main/java/org/paumard/lambdamasterclass/part1/util/CurrencyConverter.java
+++ src/main/java/org/paumard/lambdamasterclass/part1/util/CurrencyConverter.java
@@ -1,24 +1,38 @@
 package org.paumard.lambdamasterclass.part1.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toMap;
+import static org.paumard.lambdamasterclass.part1.util.CurrencyConverter.CurrencyMapHolder.MAP;
+
 public interface CurrencyConverter {
 
+    class CurrencyMapHolder {
+        static Map<String, Double> MAP;
+
+        static {
+            try {
+                MAP = Files.lines(Paths.get("./data/currency.txt"))
+                        .skip(1)
+                        .collect(toMap(
+                                line -> line.split("=")[0],
+                                line -> Double.valueOf(line.split("=")[1])
+                        ));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     static WithDate of(LocalDate date) {
-        return from -> to -> CONVERTERS.get(date).get(from).get(to);
+        return from -> to -> amount -> amount * MAP.get(to) / MAP.get(from);
     }
 
     double convert(double amount);
-
-    Map<LocalDate, Map<String, Map<String, CurrencyConverter>>> CONVERTERS = Map.of(
-            LocalDate.of(2018, 11, 5),
-            Map.of(
-                    "EUR", Map.of("GBP", (it) -> it * 0.87749d),
-                    "NOK", Map.of("EUR", (it) -> it * 0.10507d),
-                    "CHF", Map.of("CAD", (it) -> it * 1.30665d)
-            )
-    );
 
     @FunctionalInterface
     interface WithDate {
